8265f762ab388f13d2bc652dd081237d19e6b620
Martin Hamilton <m@martinh.net>
2025-06-20 10:26:07 +0100

    sashiko slow stitch workshop

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -217,7 +217,7 @@ var store = [{
       },{
         "title": "Charnwood Eco Hub Origami Fabric Butterfly Workshop",
         "excerpt":"Origami Fabric Butterfly Workshop   Come along to the Eco Hub on Saturday May 24th 2025 from 11AM to 1PM and learn how to make beautiful butterflies out of fabric scraps and beads, then use your creations to make brooches, hair clips and more! The workshop costs £15 per person and includes all materials. You can book in person in the Hub, using the QR code on the relevant poster or here - origami fabric butterfly workshop      ","categories": [],
-        "tags": ["upcoming"],
+        "tags": ["archive"],
         "url": "/workshops-and-events/origami-fabric-butterfly-workshop/",
         "teaser": "/assets/img/origami-butterfly-workshop-may2025-thumbnail.png"
       },{
@@ -238,6 +238,12 @@ var store = [{
         "tags": ["upcoming"],
         "url": "/workshops-and-events/fabric-journal-roll-workshop/",
         "teaser": "/assets/img/thumbs/fabric-roll-workshop-july25-thumb.png"
+      },{
+        "title": "Charnwood Eco Hub Sashiko-Style Slow Stitch Workshop",
+        "excerpt":"Sashiko-Style Slow Stitch Workshop   Join us from 11AM to 1PM on Saturday 26th July 2025 to discover the mindful art of slow stitching, inspired by Japanese Sashiko.   Create a unique patch for denim, a jar topper, a framed abstract piece - or simply enjoy the meditative processs.   No experience needed. Basic sewing skills helpful but not essential. All materials provided - or bring your own item to embellish.   Slow down. Stitch memories. Take something meaningful home.   The workshop costs £15 and includes all materials. You can book in person in the Hub, using the QR code on the poster below or here - Sashiko-Style Slow Stitch Workshop poster      ","categories": [],
+        "tags": ["upcoming"],
+        "url": "/workshops-and-events/sashiko-slow-stitch-workshop-july2025/",
+        "teaser": "/assets/img/thumbs/sashiko-slow-stitch-workshop-poster-july2025-thumbnail.jpg"
       },{
         "title": "Charnwood Sustainability Fair",
         "excerpt":"The Charnwood Sustainability Fair      This year’s Sustainability Fair will take place on Sunday 21st September 2025 in Queen’s Park Loughborough from 10am-3pm, highlighting green initiatives and fostering community collaboration.   This much-anticipated event is a perfect opportunity for local businesses, organisations, and artisans to showcase their sustainable products and and eco-friendly services, and their knowledge and projects.   We will have stalls representing a variety of local sustainability and eco groups, including Transition Loughborough, Incredible Edibles, etc. sharing information on their projects, how you can get involved and how you can lower your own carbon footprint, as well as entertainment, workshops and local small businesses selling sustainable products and gifts, like Men &amp; Women in Sheds and Buckingham Bees.   Don’t miss your chance to be part of these inspiring events! Apply now to help shape a sustainable Charnwood.   ","categories": [],
---
+++ _workshops-and-events/2025-07-26-sashiko-slow-stitch-workshop-july2025.md
@@ -0,0 +1,30 @@
+---
+title: Charnwood Eco Hub Sashiko-Style Slow Stitch Workshop
+layout: single
+header:
+  show_overlay_excerpt: false
+  overlay_image: /assets/img/charnwood-eco-hub-banner.jpg
+  teaser: /assets/img/thumbs/sashiko-slow-stitch-workshop-poster-july2025-thumbnail.jpg
+  actions:
+    - label: "Donate"
+      url: "https://www.paypal.com/donate/?hosted_button_id=V54MWPK2EZGPY"
+collection: workshops-and-events
+date: 2025-07-26
+released: true
+tags:
+- upcoming
+---
+# Sashiko-Style Slow Stitch Workshop
+ 
+Join us from 11AM to 1PM on Saturday 26th July 2025 to discover the mindful art of slow stitching, inspired by Japanese Sashiko.
+
+Create a unique patch for denim, a jar topper, a framed abstract piece - or simply enjoy the meditative processs.
+
+No experience needed. Basic sewing skills helpful but not essential. All materials provided - or bring your own item to embellish.
+
+Slow down. Stitch memories. Take something meaningful home.
+
+The workshop costs £15 and includes all materials. You can book in person in the Hub, using the QR code on the poster below or here - [Sashiko-Style Slow Stitch Workshop poster](https://pay.sumup.com/b2c/QLFZ0JJC)
+
+[![Sashiko-Style Slow Stitch Workshop poster](/assets/img/sashiko-slow-stitch-workshop-poster-july2025.jpg){:width="75%"}]()
+
