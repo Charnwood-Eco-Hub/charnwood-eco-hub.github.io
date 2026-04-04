2043b24eea4bc50a4a340f27974ecdb0a500c3db
Martin Hamilton <m@martinh.net>
2026-04-04 22:30:47 +0100

    updated library of things terms and conditions

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -439,7 +439,7 @@ var store = [{
       },{
         "title": "Charnwood Eco Hub Easter Activity Day 2026",
         "excerpt":"Charnwood Eco Hub Easter Activity Day   Come to Charnwood Eco Hub from 10.30AM-1.30PM on Saturday 4th April 2026 and join us for some fun ‘make your own’ Easter activities using materials from our Scrapstore and our famous Smoothie Bike!   Learn how to repurpose materials and create something useful and beautiful for your gardens or other outdoor spaces.   The activities are suitable for children, but all children must be supervised by adults. Adults are also welcome to take part in the activities, even if they aren’t supervised by children!   You can book your place by scanning the QR code on the poster or here. Tickets for the Easter Activity Day cost £5.      ","categories": [],
-        "tags": ["upcoming"],
+        "tags": ["archive"],
         "url": "/workshops-and-events/easter-activity-day-apr26/",
         "teaser": "/assets/img/thumbs/easter-activity-day-apr26.png"
       },{
--- _workshops-and-events/2026-04-04-easter-activity-day-apr26.md
+++ _workshops-and-events/2026-04-04-easter-activity-day-apr26.md
@@ -9,7 +9,7 @@ collection: workshops-and-events
 date: 2026-04-04
 released: true
 tags:
-- upcoming
+- archive
 ---
 # Charnwood Eco Hub Easter Activity Day
 
