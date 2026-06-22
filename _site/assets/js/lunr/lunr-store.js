03f6821d74efa0a80600cd3a925e16417c08db52
Martin Hamilton <m@martinh.net>
2026-06-22 19:38:43 +0100

    rebuild

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -481,13 +481,13 @@ var store = [{
       },{
         "title": "Charnwood Eco Hub Upcycled Inner Tube Jewellery Workshop",
         "excerpt":"Charnwood Eco Hub Upcycled Inner Tube Jewellery Workshop   Join us on Saturday June 13th 2026, from 11am-1pm at Charnwood Eco Hub, SOFA Building, Clarence Street, Loughborough, LE11 1DY   Get creative and give new life to old materials at our Inner Tube Upcycled Jewellery Workshop!   In this relaxed, hands-on session you’ll learn how to transform reclaimed bicycle inner tubes into bold, stylish jewellery and accessories to take home. Perfect for beginners and crafty creatives alike, with all materials supplied by Two Wheel Campus.   You can book your place by scanning the QR code on the poster or click here. The workshop costs £10 per person for students, low income, retired or Eco Hub members, £15 per person standard price.   .   ","categories": [],
-        "tags": ["upcoming"],
+        "tags": ["archive"],
         "url": "/workshops-and-events/upcycled-innertube-workshop-jun26/",
         "teaser": "/assets/img/thumbs/upcycled-innertube-workshop-jun26.png"
       },{
         "title": "Charnwood Eco Hub Plant Swap",
         "excerpt":"Charnwood Eco Hub Plant Swap   Join us on Saturday 20th June 2026 from 11am-1pm at Charnwood Eco Hub, SOFA Building, Clarence Street, Loughborough, LE11 1DY for our first Plant Swap. It’s free, and no need to book.   Got spare seedlings, cuttings, houseplants, herbs, or garden plants?   Bring them along to our friendly community Plant Swap and exchange them for something new!   Come along for a chat, share gardening tips, and help support a greener, more sustainable future.   Please make sure any plants you bring are healthy and happy!      ","categories": [],
-        "tags": ["upcoming"],
+        "tags": ["archive"],
         "url": "/workshops-and-events/plant-swap/",
         "teaser": "/assets/img/thumbs/plant-swap-jun26.png"
       },{
